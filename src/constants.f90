93bd552f0e7875d59f24de96978cfe3201fa97f5
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-12-06 17:38:04 -0300

    using iso_fortran_env for precission

--- src/constants.f90
+++ src/constants.f90
@@ -1,4 +1,5 @@
 module constants
+   use iso_fortran_env, only: real32, real64, real128
    implicit none
-   integer, parameter :: pr = 16
+   integer, parameter :: pr = real64
 end module constants
