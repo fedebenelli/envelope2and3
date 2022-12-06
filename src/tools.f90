34494446c263ab354f88a1eea5aad222f7ae914b
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-12-06 17:41:47 -0300

    reordered some modules

--- src/tools.f90
+++ src/tools.f90
@@ -1,12 +1,13 @@
 module file_operations
+   !! General use file operation procedures
    implicit none
    integer :: out_i !! ID of an output file
 
 contains
 
    character(len=200) function outfile(str, id)
-        !!From an output file name and id return a string with the style:
-        !!  <name><id>.txt
+      !!From an output file name and id return a string with the style:
+      !!  <name><id>.txt
       character(len=200), intent(in) :: str !! Generic name
       integer, intent(in) :: id !! Output file ID
 
@@ -17,102 +18,16 @@ contains
 end module file_operations
 
 module array_operations
+   !! General array operations
    use constants
    implicit none
 
 contains
 
-   subroutine find_cross(tv1, tv2, pv1, pv2, crossings)
-      use dtypes, only: cross
-      !! Find crossings between two given lines
-      !!
-      !! Returns an array of crossigns, containings the crosses found. Each row
-      !! contains the data from each found cross
-      !!
-      !!  | --------| ------- | ---------------- | ----------------- |
-      !!  | x_cross | y_cross | first_line_index | second_line_index |
-      !!  | --------| ------- | ---------------- | ----------------- |
-      !!
-
-      real(8), allocatable, intent(in)  :: tv1(:)  !! First line x values
-      real(8), allocatable, intent(in)  :: tv2(:)  !! Second line x values
-      real(8), allocatable, intent(in)  :: pv1(:)  !! First line y values
-      real(8), allocatable, intent(in)  :: pv2(:)  !! Second line y values
-
-      type(cross), allocatable :: crossings(:) !! Array of crossings
-      type(cross) :: current_cross
-
-      real(wp) :: x11, x12, x21, x22, y11, y12, y21, y22
-
-      real(wp) :: x_cross, y_cross, m1, b1, m2, b2, xlow, xup, ylow, yup
-      real(wp), dimension(2) :: xpair_1, xpair_2, ypair_1, ypair_2
-      integer :: i, j, n
-
-      if (allocated(crossings)) then
-         deallocate (crossings)
-      end if
-
-      allocate (crossings(0))
-      n = 0
-
-      do i = 2, size(tv1)
-         xpair_1 = tv1(i - 1:i)
-         ypair_1 = pv1(i - 1:i)
-
-         x11 = xpair_1(1)
-         x12 = xpair_1(2)
-         y11 = ypair_1(1)
-         y12 = ypair_1(2)
-
-         m1 = (y12 - y11)/(x12 - x11)
-         b1 = y11 - m1*x11
-
-         do j = 2, size(tv2)
-            xpair_2 = tv2(j - 1:j)
-            ypair_2 = pv2(j - 1:j)
-
-            x21 = xpair_2(1)
-            x22 = xpair_2(2)
-            y21 = ypair_2(1)
-            y22 = ypair_2(2)
-
-            m2 = (y22 - y21)/(x22 - x21)
-            b2 = y21 - m2*x21
-
-            x_cross = (b1 - b2)/(m2 - m1)
-            y_cross = m1*x_cross + b1
-
-            xlow = max(minval(xpair_1), minval(xpair_2))
-            xup = min(maxval(xpair_1), maxval(xpair_2))
-            ylow = max(minval(ypair_1), minval(ypair_2))
-            yup = min(maxval(ypair_1), maxval(ypair_2))
-
-            if ( &
-               (xlow <= x_cross) .and. (x_cross <= xup) .and. &
-               (ylow <= y_cross) .and. (y_cross <= yup) &
-               ) then
-               print *, "CROSS:", i, j, x_cross, y_cross
-               if ((abs(x_cross - crossings(n)%x) < 0.1) .and. &
-                   (abs(y_cross - crossings(n)%y) < 0.1)) then
-                  print *, "CROSS: Repeated cross, skipping..."
-                  cycle
-               end if
-
-               current_cross = cross(x_cross, y_cross, i, j)
-               n = n + 1
-
-               crossings = [crossings, current_cross]
-
-            end if
-
-         end do
-      end do
-   end subroutine find_cross
-
    subroutine append_2d(array, values)
-      real(wp), allocatable, intent(in out) :: array(:, :)
-      real(wp), allocatable, intent(in) :: values(:)
-      real(wp), allocatable :: tmp_array(:, :)
+      real(pr), allocatable, intent(in out) :: array(:, :)
+      real(pr), allocatable, intent(in) :: values(:)
+      real(pr), allocatable :: tmp_array(:, :)
 
       integer :: ni, nj, sh(2)
 
@@ -131,8 +46,28 @@ contains
 
 end module array_operations
 
+module io
+   !! I/O related procedures
+   implicit none
+
+   interface str
+      module procedure :: str_int
+   end interface
+
+contains
+   function str_int(int_in) result(str_out)
+      integer, intent(in) :: int_in
+      character(len=100) :: str_mid
+      character(len=:), allocatable :: str_out
+
+      write (str_mid, *) int_in
+      str_out = adjustl(trim(str_mid))
+   end function
+end module io
+
 module logic
-   contains
+   !! Logical procedures
+contains
    function exor(A, B) result(C)
       logical :: A, B
       logical :: C
--- src/types.f90
+++ src/types.f90
@@ -1,48 +1,118 @@
-module constants
-   implicit none
-
-   integer, parameter :: wp = kind(0.0q1)
-end module constants
-
 module dtypes
    use constants
+   use io
 
    implicit none
 
    private
    public :: envelope
+   public :: env3
    public :: cross
    public :: kfcross
-
-   type :: envelope
-      real(8), allocatable :: z(:)
-      real(8), allocatable :: t(:)
-      real(8), allocatable :: p(:)
-      real(8), allocatable :: logk(:, :)
-      type(critical_point), allocatable :: critical_points
-   end type envelope
-
+   public :: print_header
+   public :: find_cross
+   
    type :: critical_point
       real(8) :: t
       real(8) :: p
    end type critical_point
 
+   type :: envelope
+      real(pr), allocatable :: z(:) !! Global composition
+      real(pr), allocatable :: t(:) !! Temperature points
+      real(pr), allocatable :: p(:) !! Pressure points
+      real(pr), allocatable :: logk(:, :) !! ln(K) for each point
+      real(pr), allocatable :: logphi(:, :) !! lnphi for each point
+      type(critical_point), allocatable :: critical_points !! Critical points
+   contains
+      procedure :: write => write_envel_2
+   end type envelope
+
+   type, extends(envelope) :: env3
+      real(pr), allocatable :: beta(:) !! Other phase fraction
+      real(pr), allocatable :: x(:, :) !! 
+      real(pr), allocatable :: y(:, :) !! 
+      real(pr), allocatable :: w(:, :) !! 
+      real(pr), allocatable :: logks(:, :) !! ln(Ks)
+   contains
+      procedure :: write => write_envel_3
+   end type env3
+
+
    type :: cross
-      real(wp) :: x
-      real(wp) :: y
+      real(kind(1.q0)) :: x
+      real(kind(1.q0)) :: y
       integer :: i
       integer :: j
    end type cross
 
 contains
+   
+   subroutine write_envel_2(self, file_name)
+      class(envelope), intent(in):: self
+      character(len=*), optional, intent(in) :: file_name
+      character(len=:), allocatable :: filename
+      integer :: i, j, n, file_unit, n_components
+
+      if (present(file_name)) then
+         filename = file_name
+      else 
+         filename = "biphasicOUT"
+      end if
+
+      n = size(self%t)
+      n_components = size(self%z)
+
+      associate(t => self%t, p => self%p, &
+                logk => self%logk,        &
+                z => self%z &
+          )
+         open(newunit=file_unit, file=filename)
+            write(file_unit, *) "P ", "T ", ("logk" // str(i), i=1,n_components), ("z" // str(i), i=1,n_components)
+            do i=1,n
+               write(file_unit, *) p(i), t(i), logk(i, :), z
+            end do
+         close(file_unit)
+      end associate
+      deallocate(filename)
+   end subroutine write_envel_2
+
+   subroutine write_envel_3(self, file_name)
+      class(env3), intent(in):: self
+      character(len=*), optional, intent(in) :: file_name
+      character(len=:), allocatable :: filename
+      integer :: i, j, n, file_unit
+
+      if (present(file_name)) then
+         filename = file_name
+      else 
+         filename = "thriphasicOUT"
+      end if
+
+      associate(t => self%t, p => self%p, &
+          logk => self%logk, logks => self%logks, &
+          x => self%x, y => self%y, w => self%w  &
+          )
+      n = size(self%t)
+
+      open(newunit=file_unit, file=filename)
+      write(file_unit, *) "P ", "T ", ("logk ", i=1,8), ("logks ", i=1,8), ("x ", i=1,8), ("y ", i=1,8), ("w ", i=1,8)
+      do i=1,n
+         write(file_unit, *) p(i), t(i), logk(i, :), logks(i, :), x(i, :), y(i, :), w(i, :)
+      end do
+      close(file_unit)
+      end associate
+      deallocate(filename)
+   end subroutine write_envel_3
 
    function kfcross(i, t_values, logk, target_t)
       !! Estimate the Kvalues of an envelope by interpolation from a near point.
       integer, intent(in) :: i
-      real(8), allocatable, intent(in) :: t_values(:) !! Envelope's temperatures
-      real(8), allocatable, intent(in) :: logk(:, :) !! Envelope's kvalues
-      real(8), intent(in) :: target_t !! Target temperature where to interpolate
-      real(8), allocatable :: kfcross(:) !! Kvalues at desired point
+      real(pr), allocatable, intent(in) :: t_values(:) !! Envelope's temperatures
+      real(pr), allocatable, intent(in) :: logk(:, :) !! Envelope's kvalues
+      real(pr), intent(in) :: target_t !! Target temperature where to interpolate
+
+      real(pr), allocatable :: kfcross(:) !! Kvalues at desired point
 
       kfcross = (logk(i, :) - logk(i - 1, :)) &
                 /&
@@ -51,4 +121,97 @@ contains
                 + logk(i - 1, :)
    end function
 
+   subroutine print_header(name)
+      character(len=250), intent(in) :: name
+
+      print *, "==================================="
+      print *, "!", name
+      print *, "-----------------------------------"
+   end subroutine print_header
+   
+   subroutine find_cross(tv1, tv2, pv1, pv2, crossings)
+      !! Find crossings between two given lines
+      !!
+      !! Returns an array of crossigns, containings the crosses found. Each row
+      !! contains the data from each found cross
+      !!
+      !!  | --------| ------- | ---------------- | ----------------- |
+      !!  | x_cross | y_cross | first_line_index | second_line_index |
+      !!  | --------| ------- | ---------------- | ----------------- |
+      !!
+
+      real(pr), allocatable, intent(in)  :: tv1(:)  !! First line x values
+      real(pr), allocatable, intent(in)  :: tv2(:)  !! Second line x values
+      real(pr), allocatable, intent(in)  :: pv1(:)  !! First line y values
+      real(pr), allocatable, intent(in)  :: pv2(:)  !! Second line y values
+
+      type(cross), allocatable :: crossings(:) !! Array of crossings
+      type(cross) :: current_cross
+
+      real(pr) :: x11, x12, x21, x22, y11, y12, y21, y22
+
+      real(pr) :: x_cross, y_cross, m1, b1, m2, b2, xlow, xup, ylow, yup
+      real(pr), dimension(2) :: xpair_1, xpair_2, ypair_1, ypair_2
+      integer :: i, j, n
+
+      if (allocated(crossings)) then
+         deallocate (crossings)
+      end if
+
+      allocate (crossings(0))
+      n = 0
+
+      do i = 2, size(tv1)
+         xpair_1 = tv1(i - 1:i)
+         ypair_1 = pv1(i - 1:i)
+
+         x11 = xpair_1(1)
+         x12 = xpair_1(2)
+         y11 = ypair_1(1)
+         y12 = ypair_1(2)
+
+         m1 = (y12 - y11)/(x12 - x11)
+         b1 = y11 - m1*x11
+
+         do j = 2, size(tv2)
+            xpair_2 = tv2(j - 1:j)
+            ypair_2 = pv2(j - 1:j)
+
+            x21 = xpair_2(1)
+            x22 = xpair_2(2)
+            y21 = ypair_2(1)
+            y22 = ypair_2(2)
+
+            m2 = (y22 - y21)/(x22 - x21)
+            b2 = y21 - m2*x21
+
+            x_cross = (b1 - b2)/(m2 - m1)
+            y_cross = m1*x_cross + b1
+
+            xlow = max(minval(xpair_1), minval(xpair_2))
+            xup = min(maxval(xpair_1), maxval(xpair_2))
+            ylow = max(minval(ypair_1), minval(ypair_2))
+            yup = min(maxval(ypair_1), maxval(ypair_2))
+
+            if ( &
+               (xlow <= x_cross) .and. (x_cross <= xup) .and. &
+               (ylow <= y_cross) .and. (y_cross <= yup) &
+               ) then
+               print *, "CROSS:", i, j, x_cross, y_cross
+               if ((abs(x_cross - crossings(n)%x) < 0.1) .and. &
+                   (abs(y_cross - crossings(n)%y) < 0.1)) then
+                  print *, "CROSS: Repeated cross, skipping..."
+                  cycle
+               end if
+
+               current_cross = cross(x_cross, y_cross, i, j)
+               n = n + 1
+
+               crossings = [crossings, current_cross]
+
+            end if
+
+         end do
+      end do
+   end subroutine find_cross
 end module dtypes
