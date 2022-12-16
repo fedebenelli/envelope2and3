1836a38aac2f28c366adb5dfc49bd248e2e94724
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-12-16 13:41:54 -0300

    towars modernizing code, using modules instead of commons and allocatables for shared arrays

--- src/MulticompSRK_PR.f90
+++ src/MulticompSRK_PR.f90
@@ -1,23 +1,33 @@
 subroutine read2PcubicNC(nc, nin, nout)
-   implicit double precision(A - H, O - Z)
-   parameter(nco=64, RGAS=0.08314472d0)
+   !! Subroutine to read a GPECIN-like file and set up the system
+
    ! Critical constants must be given in K and bar
    ! b will be in L/mol and ac in bar*(L/mol)**2
-   double precision Kij(nco, nco), lij(nco, nco), Kinf(nco, nco), Tstar(nco, nco)
-   double precision Kinf1, Kinf2, K01, K02, lijk(nco, nco, nco)
-   dimension ac(nco), b(nco), del1(nco), rm(nco), diam(nc), Vc(nc)
+   ! -> EoS parameters will be calculated from the critical constants
+   !    to assure thermodynamic consistency
+
+   use constants
+   use system, only: setup, SRK_factory, PR_factory, z, bij, kij_mod => kij
+
+   implicit double precision(A - H, O - Z)
+   integer, parameter :: nco=64
+
+   real(pr) :: Kij(nco, nco), lij(nco, nco), Kinf(nco, nco), Tstar(nco, nco)
+   real(pr) :: ac(nco), b(nco), del1(nco), k(nco), Vc(nc)
+   real(pr) :: lijk(nco, nco, nco), kijk(nco, nco, nco)
+
    character*18 fluid(nco)
-   common/MODEL/NMODEL
-   common/Vshift/iVshift, Vs(nco)    ! added June 2016
-   common/CRIT/TC(nco), PC(nco), DCeos(nco), OM(nco)
-   common/NAMES/fluid
-   common/COMPONENTS/ac, b, del1, rm, Kij, NTdep
-   common/COVOL/b1(nco)
-   common/bcross/bij(nco, nco)
-   common/Tdep/Kinf, Tstar
-   common/bcrosscub/bijk(nco, nco, nco)
-   common/rule/ncomb
-   common/lforin/lij
+
+   common /MODEL/ NMODEL
+   common /CRIT/ TC(nco), PC(nco), DCeos(nco), OM(nco)
+   common /NAMES/ fluid
+   common /COMPONENTS/ ac, b, del1, k, Kij, NTdep
+   common /COVOL/ b1(nco)
+   common /Tdep/ Kinf, Tstar
+   common /bcrosscub/ bijk(nco, nco, nco)
+   common /rule/ ncomb
+   common /lforin/ lij
+
    read (NIN, *) ncomb, NTDEP
    Tstar = 0.d0
    third = 1.0D0/3
@@ -34,11 +44,8 @@ subroutine read2PcubicNC(nc, nin, nout)
       read (NIN, *) Tc(i), Pc(i), OM(i), Vc(i)
       dceos(i) = 1/Vc(i)
       write (nout, 1) fluid(i), Tc(i), Pc(i), Vc(i), OM(i)
-      if (iVshift == 1) then
-         read (NIN, *) ac(i), b(i), rm(i), Vs(i)
-      else
-         read (NIN, *) ac(i), b(i), rm(i)
-      end if
+      read (NIN, *) ac(i), b(i), k(i)
+
       Kij(i, i) = 0.0D0
       Lij(i, i) = 0.0D0
       if (i .gt. 1) then
@@ -51,35 +58,22 @@ subroutine read2PcubicNC(nc, nin, nout)
             Kinf(i, :i - 1) = Kinf(:i - 1, i)
             read (NIN, *) (lij(j, i), j=1, i - 1)
             lij(i, :i - 1) = lij(:i - 1, i)
-         else
-            read (NIN, *) K01, K02
-            if (NTDEP .ge. 1) read (NIN, *) Kinf1, Kinf2
-            if (NTDEP .eq. 1) read (NIN, *) Tstar1, Tstar2
-            if (NTDEP .eq. 2) read (NIN, *) C1, C2
-            read (NIN, *) Lijk(1, 1, 2), Lijk(1, 2, 2)
          end if
       end if
    end do
-!        Kinf = 0.d0   hidden bug!
+
    B1 = B
-   write (nout, *) 'Fluid     ac(bar*L2/mol2)  b(L/mol)    d1      rm'
+
+   write (nout, *) 'Fluid     ac(bar*L2/mol2)  b(L/mol)    d1      k'
    do I = 1, NC
-      write (nout, 1) fluid(i), ac(i), b(i), del1(i), rm(i)
+      write (nout, 1) fluid(i), ac(i), b(i), del1(i), k(i)
    end do
    write (NOUT, *)
    if (ncomb .lt. 2) then
       if (NTDEP .eq. 0) then
-!                  write(NOUT,*)' K12 = ',Kij(1,2)
-!                  write(NOUT,*)
          write (NOUT, *) '  Kij MATRIX'
       else
          write (NOUT, *) '  K0ij MATRIX'
-!                        write(NOUT,*)' K012 = ',Kij(1,2)
-!                        write(NOUT,*)
-!                        write(NOUT,*)' Kinf = ',Kinf
-!                        write(NOUT,*)
-!                        write(NOUT,*)'Tstar = ',Tstar
-!                        write(NOUT,*)
       end if
       do I = 1, NC
          write (NOUT, 6) FLUID(I), (Kij(j, i), j=1, i - 1)
@@ -121,6 +115,16 @@ subroutine read2PcubicNC(nc, nin, nout)
       write (NOUT, 7) Lijk(1, 1, 2), Lijk(1, 2, 2)
       write (NOUT, *)
    end if
+
+   call setup(nc, nmodel, ntdep, ncomb)
+
+   select case(nmodel)
+      case (1)
+         call SRK_factory(z, tc_in=tc, pc_in=pc, w_in=om)
+      case (2)
+         call PR_factory(z, tc_in=tc, pc_in=pc, w_in=om)
+   end select
+   
    write (NOUT, *)
    write (NOUT, *) ' Combining rules:'
    if (ncomb .eq. 0) then
@@ -131,82 +135,51 @@ subroutine read2PcubicNC(nc, nin, nout)
             bij(j, i) = bij(i, j)
          end do
       end do
-   else if (ncomb .eq. 3) then
-      do i = 1, nc
-         bijk(i, i, i) = b(i)
-         do j = i + 1, nc
-            bijk(i, i, j) = (1 - lijk(i, i, j))*(2*b(i) + b(j))/3
-            bijk(i, j, i) = bijk(i, i, j)
-            bijk(j, i, i) = bijk(i, i, j)
-            bijk(i, j, j) = (1 - lijk(i, j, j))*(b(i) + 2*b(j))/3
-            bijk(j, i, j) = bijk(i, j, j)
-            bijk(j, j, i) = bijk(i, j, j)
-            do k = j + 1, nc        ! only possible with three or more components
-               bijk(i, j, k) = (1 - lijk(i, j, k))*(b(i) + b(j) + b(k))/3
-               bijk(j, i, k) = bijk(i, j, k)
-               bijk(i, k, j) = bijk(i, j, k)
-               bijk(j, k, i) = bijk(i, j, k)
-               bijk(k, i, j) = bijk(i, j, k)
-               bijk(k, j, i) = bijk(i, j, k)
-            end do
-         end do
-      end do
-   else
-      write (NOUT, *) ' 1: Lorentz-Berthelot'
-      third = 1.0d0/3
-      do i = 1, nc
-         diam(i) = b(i)**third
-      end do
-      do i = 1, nc
-         do j = i, nc
-            bij(i, j) = ((1 - lij(i, j))*(diam(i) + diam(j))/2)**3
-            bij(j, i) = bij(i, j)
-         end do
-      end do
    end if
+
+   kij_mod = kij(:nc, :nc)
+
 1  format(A18, F8.3, 5x, F7.3, 3x, F7.3, 3x, F7.3)
-5  format(A18, F6.3)
 6  format(A18, 20F10.5)
 7  format(9x, F7.4, 2x, F7.4)
 8  format(9x, F7.2, 2x, F7.2)
 end
 
 subroutine HelmSRKPR(nc, ND, NT, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
+   use constants, only: pr, R
+   use system, only: del1, mixing_rule
+
    implicit double precision(A - H, O - Z)
-   parameter(nco=64, RGAS=0.08314472d0)
-   dimension rn(nc), Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
-   dimension dBi(nc), dBij(nc, nc)
-   dimension dDi(nc), dDij(nc, nc), dDiT(nc)
-   dimension aij(nc, nc), daijdT(nc, nc), daijdT2(nc, nc)
-   double precision Kij(nco, nco)
-   dimension ac(nco), b(nco), del1(nco), rm(nco)
-   common/COMPONENTS/ac, b, del1, rm, Kij, NTdep
-   common/rule/ncomb
+
+   real(pr) :: rn(nc), Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
+   real(pr) :: dBi(nc), dBij(nc, nc)
+   real(pr) :: dDi(nc), dDij(nc, nc), dDiT(nc)
+
    TOTN = sum(rn)
    D1 = del1(1)
    D2 = (1 - D1)/(1 + D1)
-   if (ncomb .lt. 2) then
+
+   if (mixing_rule .lt. 2) then
       call Bnder(nc, rn, Bmix, dBi, dBij)
       call DandTnder(NT, nc, T, rn, D, dDi, dDiT, dDij, dDdT, dDdT2)
-   else
-!         call Bcubicnder(nc,rn,Bmix,dBi,dBij)
-!         call DCubicandTnder(NT,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
    end if
-! The f's and g's used here are for Ar, not F (reduced Ar)                                        ***********
-! This requires to multiply by R all g, f and its derivatives as defined by Mollerup ****
+
+   ! The f's and g's used here are for Ar, not F (reduced Ar)
+   ! This requires to multiply by R all g, f and its derivatives as defined by Mollerup
    f = log((V + D1*Bmix)/(V + D2*Bmix))/Bmix/(D1 - D2)
-   g = RGAS*log(1 - Bmix/V)
+   g = R*log(1 - Bmix/V)
    fv = -1/((V + D1*Bmix)*(V + D2*Bmix))
    fB = -(f + V*fv)/Bmix
-   gv = RGAS*Bmix/(V*(V - Bmix))
+   gv = R*Bmix/(V*(V - Bmix))
    fv2 = (-1/(V + D1*Bmix)**2 + 1/(V + D2*Bmix)**2)/Bmix/(D1 - D2)
-   gv2 = RGAS*(1/V**2 - 1/(V - Bmix)**2)
-! Reduced Helmholtz Energy and derivatives
+   gv2 = R*(1/V**2 - 1/(V - Bmix)**2)
+
+   ! Reduced Helmholtz Energy and derivatives
    Ar = -TOTN*g*T - D*f
    ArV = -TOTN*gv*T - D*fv
    ArV2 = -TOTN*gv2*T - D*fv2
 
-   AUX = RGAS*T/(V - Bmix)
+   AUX = R*T/(V - Bmix)
    FFB = TOTN*AUX - D*fB
    FFBV = -TOTN*AUX/(V - Bmix) + D*(2*fv + V*fv2)/Bmix
    FFBB = TOTN*AUX/(V - Bmix) - D*(2*f + 4*V*fv + V**2*fv2)/Bmix**2
@@ -221,7 +194,7 @@ subroutine HelmSRKPR(nc, ND, NT, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn,
          end do
       end if
    end do
-! TEMPERATURE DERIVATIVES
+   ! TEMPERATURE DERIVATIVES
    if (NT .eq. 1) then
       ArT = -TOTN*g - dDdT*f
       ArTV = -TOTN*gv - dDdT*fV
@@ -230,5 +203,4 @@ subroutine HelmSRKPR(nc, ND, NT, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn,
          ArTn(i) = -g + (TOTN*AUX/T - dDdT*fB)*dBi(i) - f*dDiT(i)
       end do
    end if
-end
-
+end subroutine HelmSRKPR
--- src/ThermoRoutines_RKPR.f90
+++ src/ThermoRoutines_RKPR.f90
@@ -1,23 +1,30 @@
+!TODO: 
+!  - Termo and zTVTermo should be joined in a single subroutine with optional
+!    arguments
+
 subroutine readRKPRNC(nc, nin, nout)
    ! Critical constants must be given in K and bar
    ! b will be in L/mol and ac in bar*(L/mol)**2
    ! PARAMETER (A0=0.0017,B0=1.9681,C0=-2.7238)
    ! PARAMETER (A1=-2.4407,B1=7.4513,C1=12.504)
    ! D=[0.428363, 18.496215, 0.338426, 0.660,789.723105, 2.512392]
+   use constants
+   use system, only: bij
    implicit double precision(A - H, O - Z)
    parameter(nco=64, RGAS=0.08314472d0)
    double precision Kij(nco, nco), lij(nco, nco), Kinf(nco, nco), Tstar(nco, nco)
    dimension ac(nco), b(nco), del1(nco), rk(nco), diam(nc), vc(nc)
-   dimension D(6), Vceos(nc)
+   real(pr) :: Vceos(nc)
    character*18 fluid(nco)
-   common/CRIT/TC(nco), PC(nco), DCeos(nco), OM(nco)
-   common/NAMES/fluid
-   common/Vshift/iVshift, Vs(nco)    ! added June 2016
-   common/COMPONENTS/ac, b, del1, rk, Kij, NTDEP
-   common/rule/ncomb
-   common/bcross/bij(nco, nco)
-   common/Tdep/Kinf, Tstar
-   common/lforin/lij
+
+   common /CRIT/ TC(nco), PC(nco), DCeos(nco), OM(nco)
+   common /NAMES/ fluid
+   common /Vshift/ iVshift, Vs(nco)    ! added June 2016
+   common /COMPONENTS/ ac, b, del1, rk, Kij, NTDEP
+   common /rule/ ncomb
+   common /Tdep/ Kinf, Tstar
+   common /lforin/ lij
+
    Tstar = 0.d0
    Kinf = 0.d0
    read (NIN, *) ncomb, NTDEP
@@ -56,9 +63,6 @@ subroutine readRKPRNC(nc, nin, nout)
    end do
    write (NOUT, *)
    write (nout, *) 'Tc, Pc and Vc are given in K, bar and L/mol respectively'
-1  format('Tc=', F9.4, '   Pc =', F9.4, '   Vc =', F8.4, '   OM =', F7.4)
-3  format('Zc=', F9.4, ' Zcrat=', F9.4, ' Zceos=', F8.4, ' Vceos=', F7.4)
-2  format('ac=', F9.4, '    b =', F9.4, '  del1=', F8.4, '    k =', F7.4)
    write (NOUT, *)
    if (ncomb .lt. 2) then
       if (NTDEP .eq. 0) then
@@ -138,53 +142,67 @@ subroutine readRKPRNC(nc, nin, nout)
       end do
    end if
 
+1  format('Tc=', F9.4, '   Pc =', F9.4, '   Vc =', F8.4, '   OM =', F7.4)
+2  format('ac=', F9.4, '    b =', F9.4, '  del1=', F8.4, '    k =', F7.4)
+3  format('Zc=', F9.4, ' Zcrat=', F9.4, ' Zceos=', F8.4, ' Vceos=', F7.4)
 6  format(A18, 20F10.5)
 7  format(9x, F7.4, 2x, F7.4)
 8  format(9x, F7.2, 2x, F7.2)
 end subroutine readRKPRNC
 
-!  Then Kij values will be called indicating the lower index first, e.g. Kij(1,3)
-
-subroutine aTder(ac, Tc, rk, T, a, dadT, dadT2)
+subroutine aTder(ac, Tc, k, T, a, dadT, dadT2)
    ! Given ac,Tc and the k parameter of the RKPR correlation, as well as the actual T,
    ! this subroutine calculates a(T) and its first and second derivatives with T.
-   implicit double precision(A - H, O - Z)
-   common/MODEL/NMODEL
+   use constants
+   use system, only: thermo_model
+   implicit none
+   real(pr), intent(in) :: ac
+   real(pr), intent(in) :: Tc
+   real(pr), intent(in) :: k
+   real(pr), intent(in) :: T
+   real(pr), intent(out) :: a
+   real(pr), intent(out) :: dadT
+   real(pr), intent(out) :: dadT2
+
+   real(pr) :: Tr
+
    Tr = T/Tc
-   if (NMODEL .le. 2) then
-      rm = rk
-      a = ac*(1 + rm*(1 - sqrt(Tr)))**2
-      dadT = ac*rm*(rm - (rm + 1)/sqrt(Tr))/Tc
-      dadT2 = ac*rm*(rm + 1)/(2*Tc**2*Tr**1.5D0)
+
+   if (thermo_model .le. 2) then
+      a = ac*(1 + k*(1 - sqrt(Tr)))**2
+      dadT = ac*k*(k - (k + 1)/sqrt(Tr))/Tc
+      dadT2 = ac*k*(k + 1)/(2*Tc**2*Tr**1.5D0)
    else
-      a = ac*(3/(2 + Tr))**rk
-      dadT = -rk*a/Tc/(2 + Tr)
-      dadT2 = -(rk + 1)*dadT/Tc/(2 + Tr)
+      a = ac*(3/(2 + Tr))**k
+      dadT = -k*a/Tc/(2 + Tr)
+      dadT2 = -(k + 1)*dadT/Tc/(2 + Tr)
    end if
 end subroutine aTder
 
 subroutine aijTder(NTD, nc, T, aij, daijdT, daijdT2)
-   implicit double precision(A - H, O - Z)
-   parameter(nco=64)
-   double precision Kinf(nco, nco), Kij0(nco, nco), Kij(nco, nco), Tstar(nco, nco)
-   dimension ai(nc), daidT(nc), daidT2(nc)
-   dimension aij(nc, nc), daijdT(nc, nc), daijdT2(nc, nc)
-   dimension aux(nc, nc), ratK(nc, nc)
-   common/CRIT/TC(nco), PC(nco), DCeos(nco), OM(nco)
-   common/COMPONENTS/ac(nco), b(nco), d1(nco), rk(nco), Kij0, NTDEP
-   common/bcross/bij(nco, nco)
-   common/rule/ncomb
-   common/Tdep/Kinf, Tstar
+   use constants
+   use system, only: ac, rk => k, tc, kij0 => kij, ntdep => tdep
+   implicit none
+
+   integer, intent(in) :: ntd
+   integer, intent(in) :: nc
+   real(pr), intent(in) :: T
+   real(pr), intent(out) :: aij(nc, nc), daijdT(nc, nc), daijdT2(nc, nc)
+
+   real(pr) :: kij(nc, nc)
+
+   real(pr) :: Kinf(nc, nc), Tstar(nc, nc)
+
+   real(pr) :: ai(nc), daidT(nc), daidT2(nc)
+
+   real(pr) :: aux(nc, nc), ratK(nc, nc)
+   integer :: i, j
+
    if (NTDEP .ge. 1) then
       Kij = 0.0D0
       do i = 1, nc
          Kij(:i - 1, i) = Kinf(:i - 1, i) + Kij0(:i - 1, i)*exp(-T/Tstar(:i - 1, i))
       end do
-      !       Kij(2,1)=Kij(1,2)
-      !  ELSE IF(NTDEP.EQ.2)THEN
-      !          Kij=0.0D0
-      !          Kij(1,2)=Kij0(1,2)*exp(-T/Tstar)
-      !          Kij(2,1)=Kij(1,2)
    else
       Kij = Kij0
    end if
@@ -209,20 +227,6 @@ subroutine aijTder(NTD, nc, T, aij, daijdT, daijdT2)
          end do
       end if
    end do
-   if (ncomb .eq. 1) then
-      do i = 1, nc - 1
-         do j = i + 1, nc
-            barrgij = bij(i, j)/sqrt(b(i)*b(j))
-            aij(i, j) = barrgij*aij(i, j)
-            aij(j, i) = aij(i, j)
-            daijdT(i, j) = barrgij*daijdT(i, j)
-            daijdT(j, i) = daijdT(i, j)
-            daijdT2(i, j) = barrgij*daijdT2(i, j)
-            daijdT2(j, i) = daijdT2(i, j)
-         end do
-      end do
-   end if
-   ! Kij(:i-1, i)=Kinf+Kij0(:i-1, i)*exp(-T/Tstar(:i-1, i))
 
    if (NTDEP .ge. 1 .and. NTD .eq. 1) then
       do i = 1, nc
@@ -236,22 +240,37 @@ subroutine aijTder(NTD, nc, T, aij, daijdT, daijdT2)
    end if
 end subroutine aijTder
 
-subroutine DandTnder(NTD, nco, T, rn, D, dDi, dDiT, dDij, dDdT, dDdT2)
-   implicit double precision(A - H, O - Z)
-   ! PARAMETER (nco=2)
-   dimension rn(nco), dDiT(nco)
-   dimension dDi(nco), dDij(nco, nco)
-   dimension aij(nco, nco), daijdT(nco, nco), daijdT2(nco, nco)
-   nc = nco
+subroutine DandTnder(NTD, nc, T, rn, D, dDi, dDiT, dDij, dDdT, dDdT2)
+   use constants
+   implicit none
+
+   integer, intent(in) :: ntd
+   integer, intent(in) :: nc
+
+   real(pr), intent(in) :: T
+   real(pr), intent(in) :: rn(nc)
+   real(pr), intent(out) :: D
+   real(pr), intent(out) :: dDiT(nc)
+   real(pr), intent(out) :: dDdT
+   real(pr), intent(out) :: dDdT2
+   real(pr), intent(out) :: dDi(nc)
+   real(pr), intent(out) :: dDij(nc, nc)
+
+   real(pr) :: aij(nc, nc), daijdT(nc, nc), daijdT2(nc, nc)
+   real(pr) :: aux, aux2
+
+   integer :: i, j
+
    call aijTder(NTD, nc, T, aij, daijdT, daijdT2)
-   D = 0.0D0
-   dDdT = 0.0D0
-   dDdT2 = 0.0D0
+
+   D = 0
+   dDdT = 0
+   dDdT2 = 0
    do i = 1, nc
-      aux = 0.0D0
-      aux2 = 0.0D0
-      dDi(i) = 0.0D0
-      dDiT(i) = 0.0D0
+      aux = 0
+      aux2 = 0
+      dDi(i) = 0
+      dDiT(i) = 0
       do j = 1, nc
          dDi(i) = dDi(i) + 2*rn(j)*aij(i, j)
          if (NTD .eq. 1) then
@@ -274,13 +293,17 @@ subroutine DELTAnder(nc, rn, D1m, dD1i, dD1ij)
    parameter(nco=64)
    double precision Kij(nco, nco)
    dimension rn(nc), dD1i(nc), dD1ij(nc, nc)
-   common/COMPONENTS/ac(nco), b(nco), d1(nco), rk(nco), Kij, NTDEP
+
+   common /COMPONENTS/ ac(nco), b(nco), d1(nco), rk(nco), Kij, NTDEP
+
    D1m = 0.0D0
    do i = 1, nc
       D1m = D1m + rn(i)*d1(i)
    end do
+
    TOTN = sum(rn)
    D1m = D1m/totn
+
    do i = 1, nc
       dD1i(i) = (d1(i) - D1m)/totn
       do j = 1, nc
@@ -290,13 +313,17 @@ subroutine DELTAnder(nc, rn, D1m, dD1i, dD1ij)
 end subroutine DELTAnder
 
 subroutine Bnder(nc, rn, Bmix, dBi, dBij)
+   use constants
+   use system, only: bij
    implicit double precision(A - H, O - Z)
-   parameter(nco=64)
-   dimension rn(nc), dBi(nc), dBij(nc, nc), aux(nc)
-   common/bcross/bij(nco, nco)
+
+   real(pr) :: rn(nc), dBi(nc), dBij(nc, nc), aux(nc)
+   ! common /bcross/ bij
+
    TOTN = sum(rn)
    Bmix = 0.0D0
    aux = 0.0D0
+
    do i = 1, nc
       do j = 1, nc
          aux(i) = aux(i) + rn(j)*bij(i, j)
@@ -315,12 +342,12 @@ end subroutine Bnder
 
 subroutine HelmRKPR(nco, NDE, NTD, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
    !! Calculate the reduced residual Helmholtz Energy and it's derivatives with the RKPR EOS
+   use constants
    implicit double precision(A - H, O - Z)
    parameter(RGAS=0.08314472d0)
-   dimension :: rn(nco), Arn(nco), ArVn(nco), ArTn(nco), Arn2(nco, nco)
-   dimension dBi(nco), dBij(nco, nco), dD1i(nco), dD1ij(nco, nco)
-   dimension dDi(nco), dDij(nco, nco), dDiT(nco)
-   dimension aij(nco, nco), daijdT(nco, nco), daijdT2(nco, nco)
+   real(pr) :: rn(nco), Arn(nco), ArVn(nco), ArTn(nco), Arn2(nco, nco)
+   real(pr) :: dBi(nco), dBij(nco, nco), dD1i(nco), dD1ij(nco, nco)
+   real(pr) :: dDi(nco), dDij(nco, nco), dDiT(nco)
    common/rule/ncomb
 
    nc = nco
@@ -404,11 +431,34 @@ subroutine TERMO(nc, MTYP, INDIC, T, P, rn, V, PHILOG, DLPHIP, DLPHIT, FUGN)
    !  DLPHIP    P-derivative of ln(phi(i)) (const T, n)    (output)   INDIC < 5
    !  FUGN      comp-derivative of ln(phi(i)) (const t & P)(output)   INDIC > 2
    !  -------------------------------------------------------------------------
-   implicit double precision(A - H, O - Z)
-   parameter(RGAS=0.08314472d0)
-   real*8, dimension(nc), intent(out), optional :: PHILOG, DLPHIT, DLPHIP
-   real*8, dimension(nc, nc), intent(out), optional :: FUGN
-   dimension rn(nc), Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc), DPDN(nc)
+   use constants
+   implicit none
+   real(pr), parameter :: RGAS=0.08314472
+
+   integer, intent(in) :: nc !! Number of components
+   integer, intent(in) :: indic !! Desired element, this should be setted with optionals
+   integer, intent(inout) :: mtyp !! Type of root desired (-1 vapor, 1 liquid, 0 lower Gr)
+   real(pr), intent(in) :: t !! Temperature [K]
+   real(pr), intent(in) :: p !! Pressure [bar]
+   real(pr), intent(in) :: rn(nc) !! Mixture mole numbers
+
+   real(pr), intent(out) :: v !! Volume [L]
+   real(pr), intent(out) :: PHILOG(nc) !! ln(phi*p) vector
+   real(pr), intent(out) :: DLPHIT(nc) !! ln(phi) Temp derivative
+   real(pr), intent(out) :: DLPHIP(nc) !! ln(phi) Presssure derivative
+   real(pr), intent(out) :: FUGN(nc, nc) !! ln(phi) compositional derivative
+
+   real(pr) :: ar, arv, artv, arv2
+   real(pr) :: RT, Z, dpv, dpdt
+   real(pr) :: Arn(nc)
+   real(pr) :: ArVn(nc)
+   real(pr) :: ArTn(nc)
+   real(pr) :: Arn2(nc, nc)
+   real(pr) :: DPDN(nc)
+   real(pr) :: totn
+   integer :: ntemp, igz, nder, i, k
+
+
    !  The output PHILOG is actually the vector ln(phi(i)*P)
    NTEMP = 0
    IGZ = 0
@@ -423,7 +473,7 @@ subroutine TERMO(nc, MTYP, INDIC, T, P, rn, V, PHILOG, DLPHIP, DLPHIT, FUGN)
    call ArVnder(nc, NDER, NTEMP, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
    DPV = -ArV2 - RT*TOTN/V**2
    DPDT = -ArTV + TOTN*RGAS/V
-   
+
    do I = 1, NC
       PHILOG(I) = -log(Z) + Arn(I)/RT
       DPDN(I) = RT/V - ArVn(I)
@@ -453,15 +503,26 @@ subroutine zTVTERMO(nc, INDIC, T, rn, V, P, DPV, PHILOG, DLPHIP, DLPHIT, FUGN)
    !  DLPHIP    P-derivative of ln(phi(i)) (const T, n)    (output)  0 < INDIC < 5
    !  FUGN      comp-derivative of ln(phi(i)) (const t & P)(output)  2 < INDIC
    !  -------------------------------------------------------------------------
-   implicit double precision(A - H, O - Z)
-   parameter(RGAS=0.08314472d0)
-   real*8, dimension(nc), intent(out), optional :: PHILOG, DLPHIT, DLPHIP
-   real*8, dimension(nc, nc), intent(out), optional :: FUGN
-   dimension rn(nc), Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc), DPDN(nc)
-   ! The output PHILOG is actually the vector ln(phi(i)*P)
+   use constants
+   implicit none
+   real(pr), parameter :: RGAS=0.08314472
+
+   integer, intent(in) :: nc, indic
+   real(pr), intent(in) :: t, rn(nc), v
+
+   real(pr), intent(out) :: p, dpv
+   real(pr), intent(out) :: PHILOG(nc), DLPHIT(nc), DLPHIP(nc)
+   real(pr), intent(out) :: FUGN(nc, nc)
+
+   real(pr) :: Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc), DPDN(nc), totn
+   real(pr) :: ar, arv, artv, arv2, RT, Z, dpdt
+
+   integer :: ntemp, igz, nder, i, k
+
    NTEMP = 0
    IGZ = 0
    NDER = 1
+
    if (INDIC .gt. 2) NDER = 2
    if (INDIC .eq. 2 .or. INDIC .eq. 4) NTEMP = 1
 
@@ -497,9 +558,16 @@ subroutine zTVTERMO(nc, INDIC, T, rn, V, P, DPV, PHILOG, DLPHIP, DLPHIT, FUGN)
 end subroutine zTVTERMO
 
 subroutine PUREFUG_CALC(nc, icomp, T, P, V, phi)
-   implicit double precision(A - H, O - Z)
-   parameter(RGAS=0.08314472d0) !nc=2,
-   dimension rn(nc), Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
+   use constants
+   implicit none
+   real(pr), parameter :: RGAS=0.08314472
+   integer, intent(in) :: nc
+   integer, intent(in) :: icomp
+   real(pr), intent(in) :: T, P, V
+   real(pr), intent(out) :: phi
+
+   real(pr) :: rn(nc), Ar, Arv, ArTV, ArV2, Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
+   real(pr) :: RT, Z, philog
    rn = 0.0
    rn(icomp) = 1.0
    RT = RGAS*T
@@ -507,10 +575,9 @@ subroutine PUREFUG_CALC(nc, icomp, T, P, V, phi)
    call ArVnder(nc, 0, 0, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
    PHILOG = -log(Z) + Arn(icomp)/RT
    phi = exp(PHILOG)
-   return
 end subroutine purefug_calc
 
-subroutine VCALC(ITYP, nc, NTEMP, rn, T, P, V)
+recursive subroutine VCALC(ITYP, nc, NTEMP, rn, T, P, V)
    ! ROUTINE FOR CALCULATION OF VOLUME, GIVEN PRESSURE
 
    ! INPUT:
@@ -523,11 +590,11 @@ subroutine VCALC(ITYP, nc, NTEMP, rn, T, P, V)
 
    ! OUTPUT:
    ! V:           VOLUME
-
+   use constants
    implicit double precision(A - H, O - Z)
-   parameter(RGAS=0.08314472d0) ! nc=2,
-   dimension rn(nc), dBi(nc), dBij(nc, nc)
-   dimension Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
+   parameter(RGAS=0.08314472d0)
+   real(pr) ::  rn(nc)
+   real(pr) ::  Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
    logical FIRST_RUN
    NDER = 0
    FIRST_RUN = .true.
@@ -549,29 +616,35 @@ subroutine VCALC(ITYP, nc, NTEMP, rn, T, P, V)
 
 100 continue
 
-   V = CPV/ZETA
-   ITER = ITER + 1
-   call ArVnder(nc, NDER, NTEMP, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
-   PCALC = TOTN*RGAS*T/V - ArV
+   DEL = 1
+   pcalc = 2*p
 
-   if (PCALC .gt. P) then
-      ZETMAX = ZETA
-   else
-      ZETMIN = ZETA
-   end if
+   do while(abs(DEL) > 1d-10)
+      V = CPV/ZETA
+      ITER = ITER + 1
+      call ArVnder(&
+         nc, NDER, NTEMP, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2 &
+      )
+      PCALC = TOTN*RGAS*T/V - ArV
 
-   AT = (Ar + V*P)/(T*RGAS) - TOTN*log(V)
-   ! AT is something close to Gr(P,T)
-   DER = (ArV2*V**2 + TOTN*RGAS*T)*S3R  ! this is dPdrho/B
-   DEL = -(PCALC - P)/DER
-   ZETA = ZETA + max(min(DEL, 0.1D0), -.1D0)
-   if (ZETA .gt. ZETMAX .or. ZETA .lt. ZETMIN) &
-      ZETA = .5D0*(ZETMAX + ZETMIN)
+      if (PCALC .gt. P) then
+         ZETMAX = ZETA
+      else
+         ZETMIN = ZETA
+      end if
 
-   if (abs(PCALC - P) .lt. 1D-12) goto 101
-   if (abs(DEL) .gt. 1D-10) goto 100
+      AT = (Ar + V*P)/(T*RGAS) - TOTN*log(V)
+      ! AT is something close to Gr(P,T)
 
-101 if (ITYP .eq. 0) then
+      DER = (ArV2*V**2 + TOTN*RGAS*T)*S3R  ! this is dPdrho/B
+      DEL = -(PCALC - P)/DER
+      ZETA = ZETA + max(min(DEL, 0.1D0), -.1D0)
+
+      if (ZETA .gt. ZETMAX .or. ZETA .lt. ZETMIN) &
+         ZETA = .5D0*(ZETMAX + ZETMIN)
+   end do
+
+   if (ITYP .eq. 0) then
       ! FIRST RUN WAS VAPOUR; RERUN FOR LIQUID
       if (FIRST_RUN) then
          VVAP = V
@@ -587,9 +660,19 @@ subroutine VCALC(ITYP, nc, NTEMP, rn, T, P, V)
 end subroutine vcalc
 
 subroutine ArVnder(nc, NDER, NTD, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
-   implicit double precision(A - H, O - Z)
-   dimension rn(nc), Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
-   common/MODEL/NMODEL
+   use constants
+   use system, only: nmodel => thermo_model
+   implicit none
+   integer, intent(in) :: nc
+   integer, intent(in) :: nder
+   integer, intent(in) :: ntd
+
+   real(pr), intent(in) :: rn(nc)
+   real(pr), intent(in) :: V
+   real(pr), intent(in) :: T
+   real(pr), intent(out) :: ar, arv, artv, arv2
+   real(pr), intent(out) :: Arn(nc), ArVn(nc), ArTn(nc), Arn2(nc, nc)
+
    if (NMODEL .le. 2) then
       ! SRK or PR
       call HelmSRKPR(nc, NDER, NTD, rn, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
@@ -611,17 +694,10 @@ subroutine Bcalc(nc, x, T, BMIX)
    ! This general subroutine provides the "co-volume" for specified composition,
    ! that will be used by Evalsecond or Vcalc
    implicit double precision(A - H, O - Z)
-   parameter(MAXC=64, RGAS=0.08314472d0)
    dimension x(nc), dBi(nc), dBij(nc, nc)
-   ! DIMENSION DD(0:3,MAXC),DDT(0:3,MAXC),DTT(0:3,MAXC),DIA(MAXC),DDB(0:3,MAXC)
    common/MODEL/NMODEL
-   ! common/covol/VX(nc)  ! ESD
-   ! common/MOL/DC(2),D(2),DT(2),HA(2),HB(2)
-   ! common/MIXT/VCPM,CMIX,CVYM,CVYMT,dCVYM(MAXC),dCMIX(MAXC),
-   !     *        dCVYMT(MAXC),d2CVYM(MAXC,MAXC),d2CMIX(MAXC,MAXC)
    common/MIXRULE/NSUB
    common/BMIX/B
-   ! common/forB/DDB
    common/NG/NGR
    common/rule/ncomb
    NG = NGR
